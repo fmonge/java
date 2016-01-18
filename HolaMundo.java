92e3a6b431ba87d4c98376438f65818566f865cd
fmonge <fabian1mg@gmail.com>
2016-01-17 22:43:18 -0600

    Capítulo 0 de intro a java

---
+++ HolaMundo.java
@@ -0,0 +1,19 @@
+/*
+Para compilar hacer:
+apt-get install 
+javac HolaMundo.java
+java HolaMundo
+
+*/
+
+import java.io.*;
+
+class HolaMundo
+{
+	public static void main(String []ar)
+    {
+    	System.out.println("Hola mundo\n"); // Envía el mensaje "Hola mundo"
+        // a la salida estándar, la pantalla
+    }
+}
+
